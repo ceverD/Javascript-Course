69f093f309b37c1d388476d66a77a2479607179e
César Verde <cverde@senace.gob.pe>
2024-06-12 09:37:36 -0500

    Actividad 1 - Ejercicio 1

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,6 @@
-console.log("hello world");
+let nombre = "César Verde"; 
+let profesion = "desarrollo de software";
+let cargo = "Arquitecto de Software";
+let saludo = `Hola, me llamo ${nombre} y me dedico a ${profesion}. Estoy cursando este Máster porque me gustaría trabajar en ${cargo}`;
 
-
-console.log("laater");
+console.log(saludo);
