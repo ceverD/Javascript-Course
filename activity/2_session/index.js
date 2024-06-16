80d2f1e57a4fad9b495bd6376a575aeeb0ee33af
César Verde <cever.info@gmail.com>
2024-06-15 22:46:20 -0500

    Actividad 2 - Ejercicio 2

--- activity/2_session/index.js
+++ activity/2_session/index.js
@@ -13,6 +13,47 @@ const personKeys = getKeys(person);
 console.log(personKeys); // [ 'name', 'age', 'profession' ]
 
 // Ejercicio 2
+// 1.	En el ámbito global:
+function foo() {
+  this.a = 2;
+}
+foo();
+console.log(a); // 2
+
+// 2.	En el contexto de un objeto:
+const obj = {
+  foo: function() {
+      this.a = 2;
+  }
+};
+obj.foo();
+console.log(obj.a); // 2
+
+// 3.	Usando .call(), .apply() o .bind():
+const bar = function() {
+  console.log(this.baz);
+};
+bar.call({ baz: 1 }); // 1
+
+// 4.	En una función constructora (usando new):
+function Point2D(x, y) {
+  this.x = x;
+  this.y = y;
+}
+const p1 = new Point2D(1, 2);
+console.log(p1.x); // 1
+console.log(p1.y); // 2
+
+// 5.	En tiempo de ejecución, según el contexto actual:
+const Cat = function(name, sound) {
+  this.name = name;
+  this.sound = sound;
+  this.makeSound = function() {
+      console.log(this.sound);
+  };
+};
+const myCat = new Cat("Miau", "¡Miau!");
+myCat.makeSound(); // ¡Miau!
 
 // Ejercicio 3
 
