0ebe057a76907ada54ec426b3410e591d60df381
César Verde <cverde@senace.gob.pe>
2024-06-20 10:00:21 -0500

    Actividad 2 - Ejercicio 1

--- activity/typescript/index.js
+++ activity/typescript/index.js
@@ -1,4 +1,16 @@
 // Ejercicio 1
+var personToArray = function (person) {
+    return [person.name, person.age, person.profession];
+};
+var lucas = {
+    name: 'Lucas',
+    age: 28,
+    profession: 'Full Stack',
+};
+var result = personToArray(lucas);
+console.log(result); // ["Lucas", 28, "Full Stack"]
+
+
 // Ejercicio 2
 // Ejercicio 3
 // Ejercicio 4
--- activity/typescript/index.ts
+++ activity/typescript/index.ts
@@ -1,4 +1,22 @@
 // Ejercicio 1
+interface Person {
+    name: string;
+    age: number;
+    profession: string;
+}
+
+const personToArray = (person: Person): (string | number)[] => {
+    return [person.name, person.age, person.profession];
+};
+
+const lucas: Person = {
+    name: 'Lucas',
+    age: 28,
+    profession: 'Full Stack',
+};
+
+const result = personToArray(lucas);
+console.log(result); // ["Lucas", 28, "Full Stack"]
 
 // Ejercicio 2
 
