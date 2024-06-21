e671f3b179cb77862e29d67719f885593bcd5b4b
César Verde <cever.info@gmail.com>
2024-06-20 22:38:37 -0500

    Actividad 3 - Ejercicio 2

--- activity/typescript/index.js
+++ activity/typescript/index.js
@@ -10,7 +10,21 @@ var lucas = {
 var result = personToArray(lucas);
 console.log(result); // ["Lucas", 28, "Full Stack"]
 
-
 // Ejercicio 2
+function sumOrConcatenate(a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+    else if (typeof a === 'string' && typeof b === 'string') {
+        return "".concat(a, "-").concat(b);
+    }
+    else {
+        return "".concat(a, ":").concat(b);
+    }
+}
+console.log(sumOrConcatenate(4, 3)); // --> 7
+console.log(sumOrConcatenate(4, "hello")); // --> 4:hello 
+console.log(sumOrConcatenate("hello", "world")); // --> hello-world
+
 // Ejercicio 3
 // Ejercicio 4
--- activity/typescript/index.ts
+++ activity/typescript/index.ts
@@ -19,6 +19,20 @@ const result = personToArray(lucas);
 console.log(result); // ["Lucas", 28, "Full Stack"]
 
 // Ejercicio 2
+function sumOrConcatenate(a: number | string, b: number | string): number | string {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    } else if (typeof a === 'string' && typeof b === 'string') {
+        return `${a}-${b}`;
+    } else {
+        return `${a}:${b}`;
+    }
+}
+
+console.log(sumOrConcatenate(4, 3)); // --> 7
+console.log(sumOrConcatenate(4, "hello")); // --> 4:hello 
+console.log(sumOrConcatenate("hello", "world")); // --> hello-world
+
 
 // Ejercicio 3
 
